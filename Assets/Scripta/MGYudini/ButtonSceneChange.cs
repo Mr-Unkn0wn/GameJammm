5048349090a77fc2eb0e1f2deee66f793aee2372
yudini <74151900+dapel00@users.noreply.github.com>
2021-11-21 15:49:54 +0100

    finished Scenes
    cool

--- Assets/Scripta/MGYudini/ButtonSceneChange.cs
+++ Assets/Scripta/MGYudini/ButtonSceneChange.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonSceneChange : MonoBehaviour
 {
+    public Text text;
     public void ButtonFunc()
     {
         Debug.Log("kekekek");
         SceneManager.LoadScene("MG_Yudini");
+    }
+
+    public void goNext()
+    {
+        if (int.Parse(text.text) < 20000) SceneManager.LoadScene("LoserScene");
+        else SceneManager.LoadScene("WinnerScene");
     }
 }
